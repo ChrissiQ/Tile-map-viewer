86519856f2a9819538e5ff292dd5f27f8479b4bb
rediahs@gmail.com <rediahs@gmail.com>
2012-02-08 03:21:18 -0700

    Now using canvas element.  A million times better and faster.

--- index.js
+++ index.js
@@ -1,107 +1,75 @@
 function coord(x,y){
+	// Coordinate object.  If given coords are blank, use default of 0,0.
 	this.x = x ? x : 0;
 	this.y = y ? y : 0;
 }
 
-var world = new function(){
-	this.land = new Array();
-	
-	this.createMap = function(){
-		this.maxX = 50;
-		this.maxY = 20;
-		
-		for ( i=0 ; i<this.maxX ; i++){
-			this.land[i] = [];
-			for ( j=0 ; j<this.maxY ; j++){
-				this.land[i][j] = 0;
-			}
-			
-		}
-	}
-	
-	this.drawMap = function(){
-		this.worldText = "";
-		for ( i=0 ; i<this.maxY ; i++ ){
-			
-			this.worldText += "<div class='clear' id='y" + i + "'>";
-			for ( j=0 ; j<this.maxX ; j++ ){
-				if (this.land[j][i] == 1){
-					this.worldText += "<span id='x" + j + "' class='land'></span>";
-				} else {
-					this.worldText += "<span id='x" + j + "' class='land'></span>";
-				}
-			}
-			this.worldText += "</div>";
-			
-		}
-		$("#inner").html(this.worldText);
-	}
-	
-	this.createLandMass = function(){
-		
-		var randStart = new coord(
-			Math.floor( Math.random() * (this.maxX) ),
-			Math.floor( Math.random() * (this.maxY) )
-		);
-		this.land[randStart.x][randStart.y] = 1;
-		
-		document.getElementById("y" + randStart.y)
-			.childNodes.item(
-				document.getElementById("x" + randStart.x))
-				.setAttribute("class", "water");
+var world = new Array();
+var max = new coord(150,150);
 
-		//$("#y" + randStart.y + " #x" + randStart.x).removeClass("land").addClass("water");
-		
-		var current = new coord(randStart.x, randStart.y);
-		for ( i = 1 ; i < Math.floor((this.maxX * this.maxY)/2) ; i++ ){
-				
-			while (this.land[current.x][current.y] == 1) {
-				
-				// Add an integer between -1 and 1 to each coord.
-				current.x += Math.floor(Math.random() *3 -1);
-				current.y += Math.floor(Math.random() *3 -1);
-				
-				if (current.x >= this.maxX) current.x = this.maxX-1;
-				if (current.y >= this.maxY) current.y = this.maxY-1;
-				if (current.x < 0) current.x = 0;
-				if (current.y < 0) current.y = 0;
-			}
-			this.land[current.x][current.y] = 1;
-			//document.getElementById("y" + current.y)
-			//.childNodes.item(
-			//	document.getElementById("x" + current.x))
-			//	.setAttribute("class", "water");
-			console.log(current, $("#y" + current.y + " #x" + current.x), document.getElementById("y" + current.y),
-				document.getElementById("y" + current.y).childNodes.item(
-				document.getElementById("x" + current.x)));
-			$("#y" + current.y + " #x" + current.x).removeClass("land").addClass("water");
+// Create flat blank world.
+(function flatWorld(){
+	for (i=0; i<max.x; i++){
+		world[i] = [];
+		for (j=0; j<max.y; j++){
+			world[i][j] = 0;
 		}
 	}
+}());
+// Fill world with interesting stuff.
+(function fillWorld(){
+	var randStart = new coord(
+		Math.floor( Math.random() * (max.x) ),
+		Math.floor( Math.random() * (max.y) )
+	);
+	
+	var current = new coord(randStart.x, randStart.y);
+	for ( i = 1 ; i < Math.floor((max.x * max.y)/2) ; i++ ){		
+		while (world[current.x][current.y] == 1) {
+			
+			// Add an integer between -1 and 1 to each coord.
+			current.x += Math.floor(Math.random() *3 -1);
+			current.y += Math.floor(Math.random() *3 -1);
+			
+			// If the coord has left the map, bring it back on.
+			if (current.x >= max.x) current.x = max.x-1;
+			if (current.y >= max.y) current.y = max.y-1;
+			if (current.x < 0) current.x = 0;
+			if (current.y < 0) current.y = 0;
+		}
+		
+		// Flip the switch to water on that pixel.
+		world[current.x][current.y] = 1;
+	}
+}());
+
+var scale = 4;
+
+
+
+var canvas = document.getElementById('game');
+if (canvas.getContext){
+	
+	var ctx = canvas.getContext('2d');
+	canvas.width = max.x * scale;
+	canvas.height = max.y * scale;
+
+	for (j=0; j<max.y; j++){
+		for (i=0; i<max.x; i++){
+			if (world[i][j] == 0){
+				ctx.fillStyle = "blue";
+			} else {
+				ctx.fillStyle = "green";
+			}
+			ctx.fillRect(i*scale,j*scale,scale,scale);	
+		}
+	}
+} else {  
+  // canvas-unsupported code here  
 }
 
-world.createMap();
-world.drawMap();
-world.createLandMass();
+
 
 $(document).keydown(function(event){
 	$("#keyPressed").text(event.keyCode)
 });
-
-
-
-
-/*		var divHeight = $("#inner").height();
-		var divWidth = $("#inner").width();
-		
-		var windowHeight = $(window).height();
-		var windowWidth = $(window).width();
-		
-		if (windowHeight > divHeight){
-			
-			$("#inner").height(divHeight);
-			$("#inner").css("margin-top", "-" + Math.floor(divHeight/2).toString() + "px");
-		} else {
-			$("#inner").height(windowHeight);
-			$("#inner").css("margin-top", "-" + Math.floor(windowHeight/2).toString() + "px");
-		}
-*/
